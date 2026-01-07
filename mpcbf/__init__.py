f3aff1f3a98b2061e1d61dccb81eaad19d67135c
Taekyung Kim <taekyung@umich.edu>
2026-01-07 02:56:42 -0500

    add-feat: initial implementaiton on pcbf

--- mpcbf/__init__.py
+++ mpcbf/__init__.py
@@ -6,7 +6,22 @@ This package provides:
 - Drifting environment with racing track simulation
 - Car dynamics based on kinematic bicycle model
 - Integration with safe_control library
+- Policy Control Barrier Function (PCBF) implementation in JAX
 """
 
 __version__ = "0.1.0"
+
+from mpcbf.pcbf import (
+    PCBF, 
+    DriftingCarDynamicsJAX,
+    LaneChangeControllerJAX,
+    StoppingControllerJAX,
+)
+
+__all__ = [
+    'PCBF', 
+    'DriftingCarDynamicsJAX',
+    'LaneChangeControllerJAX',
+    'StoppingControllerJAX',
+]
 
